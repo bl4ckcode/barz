1eac4b7d7fad5d7df1ae8a735d66b92e80704bed
Carlos Alves <carlosaugusto.alves-ext@ab-inbev.com>
2024-11-20 15:00:57 -0300

    - Fix Firebase auth - Upgrade minSdkVersion to 23

--- lib/features/authentication/auth_injection.dart
+++ lib/features/authentication/auth_injection.dart
@@ -1,16 +1,23 @@
-import 'package:barz/core/network/dio_network.dart';
 import 'package:barz/core/utils/injections.dart';
 import 'package:barz/features/authentication/data/data_sources/abstract_login_api.dart';
 import 'package:barz/features/authentication/data/data_sources/login_api.dart';
 import 'package:barz/features/authentication/data/repositories/login_repository.dart';
 import 'package:barz/features/authentication/domain/repositories/abstract_login_repository.dart';
 import 'package:barz/features/authentication/domain/usecases/login_usecase.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 initLoginInjections() {
-  getItInjector
-      .registerSingleton<AbstractLoginApi>(LoginApi(DioNetwork.appAPI));
+  // Register FirebaseAuth as a singleton
+  getItInjector.registerSingleton<FirebaseAuth>(FirebaseAuth.instance);
+
+  // Register LoginApi with FirebaseAuth dependency
+  getItInjector.registerSingleton<AbstractLoginApi>(
+      LoginApi(getItInjector<FirebaseAuth>()));
+
+  // Register LoginRepository with LoginApi dependency
   getItInjector.registerSingleton<AbstractLoginRepository>(
       LoginRepositoryImpl(getItInjector()));
-  // getItInjector.registerSingleton<HomeSharedPrefs>(HomeSharedPrefs(getItInjector()));
+
+  // Register LoginUseCase with LoginRepository dependency
   getItInjector.registerSingleton<LoginUsecase>(LoginUsecase(getItInjector()));
 }
