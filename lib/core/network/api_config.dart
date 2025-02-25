27fc8f36fca25bbb0c844c2fa26ce12963509bed
Carlos Alves <carlosaugusto.alves-ext@ab-inbev.com>
2025-02-25 20:01:04 -0300

    - Finish authentication business layer - Back-end repo created same date

--- lib/core/network/api_config.dart
+++ lib/core/network/api_config.dart
@@ -1,4 +1,4 @@
-const String baseUrl = "http://127.0.0.1:8000/";
+const String baseUrl = "http://127.0.0.1:8000//";
 
 Map<String, String> getHeaders() {
   return {
--- lib/features/authentication/domain/repositories/abstract_login_repository.dart
+++ lib/features/authentication/domain/repositories/abstract_login_repository.dart
@@ -3,7 +3,13 @@ import 'package:barz/features/authentication/domain/models/login_params.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class AbstractLoginRepository {
-  Future<Either<Failure, String?>> login(LoginParams params);
+  Future<Either<Failure, String?>> loginWithPhone(LoginParams params);
+
+  Future<Either<Failure, String?>> loginWithGoogle(LoginParams params);
+
+  Future<Either<Failure, String?>> loginWithApple(LoginParams params);
+
+  Future<Either<Failure, String?>> loginWithFacebook(LoginParams params);
 
   Future<Either<Failure, String?>> verifySmsCode({
     required String verificationId,
